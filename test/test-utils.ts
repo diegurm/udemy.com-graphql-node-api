a89c873bb8aab9807416137e6c5587bb744eac3b
Diego Rodrigues <diegurm@hotmail.com>
2019-09-01 22:26:12 -0300

    Auto stash before merge of "master" and "github/master"

---
+++ test/test-utils.ts
@@ -0,0 +1,20 @@
+import * as chai from 'chai';
+const chaiHttp = require('chai-http');
+import app from '../src/app';
+import db from '../src/models';
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+const handleError = error => {
+    const menssage: string = (error.response) ? error.response.res.text : error.message || error;
+    return Promise.reject(`${error.name}:${menssage}`);
+}
+
+export {
+    app,
+    db,
+    chai,
+    expect,
+    handleError
+}
